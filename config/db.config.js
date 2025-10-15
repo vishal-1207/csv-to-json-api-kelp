94c006ad030860a8b4d50dec094a65d8a2a52370
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-10-15 20:18:21 +0530

    added database config file

---
+++ config/db.config.js
@@ -0,0 +1,14 @@
+import pg from "pg";
+const { Pool } = pg;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+export const query = (text, params) => pool.query(text, params);
+
+export const getPool = () => pool;
